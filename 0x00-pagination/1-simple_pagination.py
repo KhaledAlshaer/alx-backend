1775832934f0f85bbe33ed2aecfba0b11f943a7f
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-08-19 13:30:36 +0000

    1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,17 +41,5 @@ class Server:
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data)
-            return []
+        return []
         return data[start:end]
-
-
-
-
-
-
-
-
-
-
-
-
