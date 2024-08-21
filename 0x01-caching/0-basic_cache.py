65d1bc1d7010370f8731b796b54e43c1092c337f
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-08-21 13:43:48 +0000

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,8 @@ class BasicCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """return the value in 'self.cache_data' linked to 'key'
